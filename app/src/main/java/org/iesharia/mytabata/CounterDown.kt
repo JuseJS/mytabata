90d892f0a640d511e469ae99a6a9d92d4cde3fb1
Navarro <joseanp0202@gmail.com>
2024-10-17 10:13:58 +0100

    arreglado el contador

--- app/src/main/java/org/iesharia/mytabata/CounterDown.kt
+++ app/src/main/java/org/iesharia/mytabata/CounterDown.kt
@@ -1,21 +1,40 @@
 package org.iesharia.mytabata
 
 import android.os.CountDownTimer
+import android.util.Log
 
-class CounterDown(var segundos: Int, var loQueHaceAlHacerTick: (Long) -> Unit) {
-    var myCounter : CountDownTimer
-    var counterState : Boolean = false
+class CounterDown(var segundos: Int, var loquehacealhacertick: (Long) -> Unit) {
+    private var counterState : Boolean = false
 
-    init {
-        myCounter = object : CountDownTimer((segundos * 1000L), 1000) {
+    val myCounter = object : CountDownTimer((segundos * 1000L), 1000) {
 
-            override fun onTick(millisUntilFinished: Long) {
-                loQueHaceAlHacerTick(millisUntilFinished / 1000)
-            }
-
-            override fun onFinish() {
-                counterState = false
-            }
+        override fun onTick(millisUntilFinished: Long) {
+            if (counterState) loquehacealhacertick(millisUntilFinished / 1000)
         }
+
+        override fun onFinish() {
+            counterState = false
+        }
+    }
+
+    fun toggle() {
+        Log.i("dam2", "toggle: $counterState")
+        if (this.counterState){
+            this.cancel()
+
+        } else {
+            Log.i("dam2", "toggle: start")
+            this.start()
+        }
+    }
+
+    fun start() {
+        counterState = true
+        this.myCounter.start()
+    }
+
+    fun cancel() {
+        counterState = false
+        this.myCounter.cancel()
     }
 }
--- app/src/main/java/org/iesharia/mytabata/MainActivity.kt
+++ app/src/main/java/org/iesharia/mytabata/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -38,8 +39,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Counter(modifier: Modifier = Modifier) {
     var theCounter by remember { mutableLongStateOf(0L) }
-    val miConterDown = CounterDown(99) { newvalue -> theCounter = newvalue }
-
+    var miConterDown by remember{ mutableStateOf(CounterDown(99) { newvalue ->
+        theCounter = newvalue
+    })}
 
     Column {
         Text(
@@ -47,12 +49,7 @@ fun Counter(modifier: Modifier = Modifier) {
             modifier = modifier
         )
         Button(onClick = {
-            if (!miConterDown.counterState) {
-                miConterDown.myCounter.start()
-                miConterDown.counterState = true
-            } else {
-                miConterDown.myCounter.cancel()
-            }
+            miConterDown.toggle()
         }) {
             Text(
                 text = "Pulsar"
