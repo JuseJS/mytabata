7cc52c44847d7ef6a97f80453687b395a3dd8467
Navarro <joseanp0202@gmail.com>
2024-10-17 19:24:35 +0100

    Modificada pantalla principal para mostrar la informacion de inicio

--- app/src/main/java/org/iesharia/mytabata/MainActivity.kt
+++ app/src/main/java/org/iesharia/mytabata/MainActivity.kt
@@ -4,21 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.iesharia.mytabata.ui.theme.MytabataTheme
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +37,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MytabataTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Counter(
+                    MainScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -37,31 +47,92 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Counter(modifier: Modifier = Modifier) {
-    var theCounter by remember { mutableLongStateOf(0L) }
-    var miConterDown by remember{ mutableStateOf(CounterDown(99) { newvalue ->
-        theCounter = newvalue
-    })}
+fun MainScreen(modifier: Modifier = Modifier) {
+    var intervalCount by remember { mutableIntStateOf(1) }
+    var workTime by remember { mutableIntStateOf(0) }
+    var restTime by remember { mutableIntStateOf(0) }
 
-    Column {
-        Text(
-            text = theCounter.toString(),
-            modifier = modifier
-        )
-        Button(onClick = {
-            miConterDown.toggle()
-        }) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = "Intervalos", fontSize = 24.sp)
+            Spacer(modifier = Modifier.width(16.dp))
             Text(
-                text = "Pulsar"
+                text = "➖",
+                fontSize = 24.sp,
+                modifier = Modifier.clickable { if (intervalCount > 1) intervalCount-- }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = intervalCount.toString(), fontSize = 24.sp)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "➕",
+                fontSize = 24.sp,
+                modifier = Modifier.clickable { intervalCount++ }
+            )
+        }
+        // Tiempo de Trabajo
+        Text(text = "Tiempo de Trabajo", fontSize = 24.sp, modifier = Modifier.padding(top = 24.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = "➖",
+                fontSize = 24.sp,
+                modifier = Modifier.clickable { if (workTime > 0) workTime-- }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = String.format(Locale.US, "%02d:%02d", workTime / 60, workTime % 60),
+                fontSize = 36.sp
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "➕",
+                fontSize = 24.sp,
+                modifier = Modifier.clickable { workTime++ }
+            )
+        }
+
+        // Tiempo de Descanso
+        Text(text = "Tiempo de Descanso", fontSize = 24.sp, modifier = Modifier.padding(top = 24.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = "➖",
+                fontSize = 24.sp,
+                modifier = Modifier.clickable { if (restTime > 0) restTime-- }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = String.format(Locale.US, "%02d:%02d", restTime / 60, restTime % 60),
+                fontSize = 36.sp
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "➕",
+                fontSize = 24.sp,
+                modifier = Modifier.clickable { restTime++ }
             )
         }
     }
 }
 
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CounterPreview() {
     MytabataTheme {
         Counter()
     }
